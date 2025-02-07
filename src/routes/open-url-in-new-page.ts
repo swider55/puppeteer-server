3804e88184657571ad5c0fbd0fa4b7241f94260a
Rafal <swiderski.ra@gmail.com>
2025-02-07 10:01:14 +0100

    Fix problem when headless is true

--- src/routes/open-url-in-new-page.ts
+++ src/routes/open-url-in-new-page.ts
@@ -15,6 +15,7 @@ router.post("/", async (req, res) => {
     if (!browser) return res.status(400).send("Browser not started");
 
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
 
     await page.goto(url);
 
